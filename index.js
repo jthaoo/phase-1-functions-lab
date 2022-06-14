af1dfbb3ef6cdb60cb9bf9d50b21e108c3e57d02
Johnathan Thao <johnathanthao21@gmail.com>
2022-06-14 09:29:24 -0600

    commit tree 012ba2e38e9fddf8608140bee6e3712ee86d2246

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(bStart){
+    if (bStart > 42)
+        return bStart - 42
+    else (bStart < 42)
+        return 42 - bStart
+}
+
+function distanceFromHqInFeet(fStart){
+    if (fStart > 42)
+        return (fStart-42) * 264;
+    else (fStart < 42)
+        return (42-fStart) * 264
+}
+
+function distanceTravelledInFeet(fLocation, dLocation){
+    if (fLocation > dLocation)
+        return (fLocation - dLocation) * 264
+    else (fLocation < dLocation)
+        return (dLocation - fLocation) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+  const distanceInFeet = distanceTravelledInFeet(start, destination);
+  
+    if (distanceInFeet <= 400) {
+    return 0;
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    return 0.02 * (distanceInFeet - 400);
+  } else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+    return 25;
+
+  } else {
+    return "cannot travel that far";
+  }
+}
